3888f4819a26120ad9ad3b73bb93e56e00cf7680
Yurcell <chestayurcel@gmail.com>
2025-12-18 14:30:47 +0700

    M Menonaktifkan navigasi ke halaman detail

--- app/src/main/java/com/example/praktikumlocalrestapi/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/praktikumlocalrestapi/uicontroller/PetaNavigasi.kt
@@ -29,7 +29,7 @@ fun HostNavigasi(
         composable(DestinasiHome.route) {
             HomeScreen(navigateToItemEntry = { navController.navigate(DestinasiEntry.route) },
                 navigateToItemUpdate = {
-                    navController.navigate("${DestinasiDetail.route}/${it}")
+//                    navController.navigate("${DestinasiDetail.route}/${it}")
                 })
         }
         composable(DestinasiEntry.route){
