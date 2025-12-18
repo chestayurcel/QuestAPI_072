8bf7bf2031080abc6d46cb96d079f896ab81d6bd
Yurcell <chestayurcel@gmail.com>
2025-12-18 12:11:00 +0700

    Membuat repositori untuk DataSiswa

---
+++ app/src/main/java/com/example/praktikumlocalrestapi/repositori/RepositoryDataSiswa.kt
@@ -0,0 +1,20 @@
+package com.example.praktikumlocalrestapi.repositori
+
+import com.example.praktikumlocalrestapi.apiservice.ServiceApiSiswa
+import com.example.praktikumlocalrestapi.modeldata.DataSiswa
+
+interface RepositoryDataSiswa {
+    suspend fun getDataSiswa() : List<DataSiswa>
+    suspend fun postDataSiswa(dataSiswa: DataSiswa) :retrofit2.Response<Void>
+    // suspend fun getSatuSiswa(id: Int) : DataSiswa
+    // suspend fun editSatuSiswa(
+}
+
+class JaringanRepositoryDataSiswa(
+    private val serviceApiSiswa: ServiceApiSiswa
+): RepositoryDataSiswa{
+    override suspend fun getDataSiswa(): List<DataSiswa> = serviceApiSiswa
+        .getSiswa()
+    override suspend fun postDataSiswa(dataSiswa: DataSiswa):retrofit2
+        .Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
+}
