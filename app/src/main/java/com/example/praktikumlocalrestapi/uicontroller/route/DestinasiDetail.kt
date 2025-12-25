860b46e38c70262caf48ce2c4dc4b1e37eafb41d
Yurcell <chestayurcel@gmail.com>
2025-12-25 20:41:48 +0700

    Menghapus baris kosong di DestinasiDetail

--- app/src/main/java/com/example/praktikumlocalrestapi/uicontroller/route/DestinasiDetail.kt
+++ app/src/main/java/com/example/praktikumlocalrestapi/uicontroller/route/DestinasiDetail.kt
@@ -5,7 +5,6 @@ import com.example.praktikumlocalrestapi.R
 object DestinasiDetail: DestinasiNavigasi {
     override val route = "detail_siswa"
     override val titleRes = R.string.detail_siswa
-
     const val itemIdArg = "idSiswa"
     val routeWithArgs = "$route/{$itemIdArg}"
 }
