839edaf9cffbdfa4d2fde1130398fd8bdc39aafe
Yurcell <chestayurcel@gmail.com>
2025-12-25 18:16:30 +0700

    Memperbarui repositori untuk mengambil, mengubah, dan menghapus satu data siswa

--- app/src/main/java/com/example/praktikumlocalrestapi/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/praktikumlocalrestapi/repositori/RepositoryDataSiswa.kt
@@ -6,6 +6,9 @@ import com.example.praktikumlocalrestapi.modeldata.DataSiswa
 interface RepositoryDataSiswa {
     suspend fun getDataSiswa() : List<DataSiswa>
     suspend fun postDataSiswa(dataSiswa: DataSiswa) :retrofit2.Response<Void>
+    suspend fun getSatuSiswa(id:Int) : DataSiswa
+    suspend fun editSatuSiswa(id:Int,dataSiswa: DataSiswa):retrofit2.Response<Void>
+    suspend fun hapusSatuSiswa(id:Int):retrofit2.Response<Void>
 }
 
 class JaringanRepositoryDataSiswa(
@@ -15,4 +18,9 @@ class JaringanRepositoryDataSiswa(
         .getSiswa()
     override suspend fun postDataSiswa(dataSiswa: DataSiswa):retrofit2
         .Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
+    override suspend fun getSatuSiswa(id: Int): DataSiswa = serviceApiSiswa.getSatuSiswa(id)
+    override suspend fun editSatuSiswa(id: Int,dataSiswa: DataSiswa):retrofit2.Response<Void> =
+        serviceApiSiswa.editSatuSiswa(id, dataSiswa)
+    override suspend fun hapusSatuSiswa(id: Int):retrofit2.Response<Void> = serviceApiSiswa
+        .hapusSatuSiswa(id)
 }
